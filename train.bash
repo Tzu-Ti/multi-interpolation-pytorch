aaf7895bd63dd76aedf7082a2c776b6da0a438b5
Tzu-Ti <a2699560@gmail.com>
2021-01-08 15:09:59 +0800

    2021/01/08 add 3 layers LSTM model

--- train.bash
+++ train.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 python train.py \
---save_dir checkpoints/ucf_cslstm_20201210 \
---gen_frm_dir results/ucf_cslstm_20201210 \
---log_dir ../tensorflow/logs/ucf_cslstm_20201210 \
+--save_dir checkpoints/ucf_cslstm_20210108 \
+--gen_frm_dir results/ucf_cslstm_20210108 \
+--log_dir ../tensorflow/logs/ucf_cslstm_20210108 \
 --model_name BiLSTM \
 --seq_length 11 \
 --img_size 256 \
 --img_channel 3 \
---num_hidden 32,32,64,64 \
+--num_hidden 32,32,32,32 \
 --patch_size 4 \
