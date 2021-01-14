d2a67df9a25af7675c1f75710b074cf56a8cfd29
Tzu-Ti <a2699560@gmail.com>
2021-01-14 08:46:24 +0000

    2021/01/14 add data parallel

--- .ipynb_checkpoints/train-checkpoint.bash
+++ .ipynb_checkpoints/train-checkpoint.bash
@@ -11,4 +11,4 @@ python train.py \
 --num_hidden 32,32,32 \
 --patch_size 4 \
 
-python train.py --save_dir checkpoints/ucf_cslstm_coding --gen_frm_dir results/ucf_cslstm_coding --log_dir ../tensorflow/logs/ucf_cslstm_coding --model_name Bi-LSTM3 --seq_length 7 --img_size 224 --img_channel 3 --num_hidden 32,32,32 --patch_size 4
+python train.py --save_dir checkpoints/ucf_cslstm_coding --gen_frm_dir results/ucf_cslstm_coding --log_dir ../tensorflow/logs/ucf_cslstm_coding --model_name BiLSTM-3 --seq_length 7 --img_size 224 --img_channel 3 --num_hidden 32,32,32 --patch_size 4
